c2b1debb133ed1a9a37012ecbd4666107e5f5e82
yusufyilmz <yusufyilmz@yusufs-MacBook-Pro.local>
2018-08-14 22:02:14 +0300

    added web socket

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+var socket =  io();
+socket.on('connect', () => {
+    console.log('connected');
+
+    socket.emit('createMessage', {
+        from:'yusuf',
+        text: 'hey',
+
+    })
+});
+
+socket.on('disconnect', () =>{
+    console.log('disconnected');
+
+});
+
+socket.on('newMessage', (message) =>{
+    console.log('new email', message);
+
+});
--- server/server.js
+++ server/server.js
@@ -1,13 +1,42 @@
 const path = require('path');
 const express = require('express');
-
-const publicPath = path.join(__dirname, '../public/');
-var app = express();
+const http = require('http');
 const port = process.env.PORT || 3000 ;
+const publicPath = path.join(__dirname, '../public/');
+const socketIO = require('socket.io');
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+
+io.on('connection', (socket) => {
+    
+
+
+    socket.emit('newMessage', {
+        from: 'mike@com',
+        text: 'hey whatsup',
+        createdAt : 123
+    });
+
+
+
+    socket.on('createMessage', (message) =>{
+        console.log('createEmail', message);
+    });
+
+
+    socket.on('disconnect', () =>{
+        console.log('disconnected');
+    });
+
+});
+
+
 
 app.use(express.static(publicPath));
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('server started ');
 })
